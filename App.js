d0cc6fc7c81c666bd907d12e08f3cc95e0850483
maxcheung1996 <cheungwaiman1919@gmail.com>
2022-01-11 23:40:42 +0800

    11012022

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+//import { useDimensions } from "@react-native-community/hooks";
 import {
   StyleSheet,
   Text,
@@ -10,6 +11,10 @@ import {
   TouchableHighlight,
   TouchableNativeFeedback,
   Button,
+  Alert,
+  Platform,
+  StatusBar as nativeStatusBar,
+  Dimensions,
 } from "react-native";
 
 export default function App() {
@@ -17,11 +22,15 @@ export default function App() {
     alert("Text Clicked");
   };
 
+  //console.log(useDimensions());
+
   console.log(require("./assets/icon.png"));
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button color="orange" title="Click Me" onPress={handlePress} />
+      <View
+        style={{ backgroundColor: "dodgerblue", width: "100%", height: "30%" }}
+      ></View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -31,7 +40,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? nativeStatusBar.currentHeight : 0,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
